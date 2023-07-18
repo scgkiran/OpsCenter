2c54ba8ba85b26fbf60eeb4bc5fdfc17e784cdc9
Chandra Sanapala <chandra@sundeck.io>
2023-07-18 13:58:08 +0530

    SD-4329 Setup ef_url & add setup_sundeck_tenant_url function

--- app/ui/setup.py
+++ app/ui/setup.py
@@ -74,6 +74,8 @@ def setup_block():
     sf_region_without_public = sf_region.split(".")[-1]
 
     region = region_map[sf_region_without_public]
+    external_func_url = get_api_gateway_url(region, sd_deployment)
+    connection.Connection.get().call("INTERNAL.SETUP_EF_URL", external_func_url)
 
     def expander(num: int, title: str, finished: bool) -> st.expander:
         c = "[Pending]"
